1aaa8444d6b148cc0b2e6726214f8e3a28143c5b
nishita24011998 <nishita.cairsc.mathura@gmail.com>
2022-07-21 13:33:52 +0530

    overriding changes

---
+++ 03-Inheritance/src/SuperKeyword/SuperTest.java
@@ -0,0 +1,18 @@
+package SuperKeyword;
+
+public class SuperTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Child c2 = new Child();
+		c2.displayDetails();
+		
+		Child c1 = new Child(89, 102);
+		c1.displayDetails();
+		
+		Child c = new Child(10, 20, 6, 9);
+		c.displayDetails();
+
+	}
+
+}
