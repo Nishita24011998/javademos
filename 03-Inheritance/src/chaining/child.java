ddcd7c48111f9add93332c7358c5524149f0e6ba
nishita24011998 <nishita.cairsc.mathura@gmail.com>
2022-07-23 13:37:29 +0530

    chaining changes

---
+++ 03-Inheritance/src/chaining/child.java
@@ -0,0 +1,13 @@
+package chaining;
+
+public class child extends parent{
+	child() {
+		System.out.println("Child Constructor: No Args");
+	}
+	
+	child(int x) {
+//		super(x);
+		System.out.println("Child Constructor: One Arg");
+	}
+
+}
