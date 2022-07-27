43dadb723cb011a4be103b2c0dc37bdd38c3d822
nishita24011998 <nishita.cairsc.mathura@gmail.com>
2022-07-27 11:11:39 +0530

    listDemoChanges

---
+++ S13 Arrays/src/usingarrays/SumOfArrays.java
@@ -0,0 +1,16 @@
+package usingarrays;
+
+public class SumOfArrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[] = {70,50,67};
+		int sum =0;
+		for(int i=0;i<arr.length;i++)
+		{
+			sum = sum +arr[i];
+		}
+		System.out.println(sum);
+		}
+
+}
