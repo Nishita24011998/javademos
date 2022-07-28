139eabf4983221fe9f1d5961ec8f3e496b45e4a0
nishita24011998 <nishita.cairsc.mathura@gmail.com>
2022-07-28 21:04:26 +0530

    Project Changes

---
+++ Lockedme_Phase1_Project/src/lockedme/Main.java
@@ -0,0 +1,13 @@
+package lockedme;
+
+public class Main {
+	/*Enter your desired Directory path */
+	public static final String path = "C:\\Users\\Nishita bansal\\OneDrive\\Documents\\FILES"; 
+	
+	public static void main(String[] args) {
+		Menus menu = new Menus();
+		menu.introScreen();
+		menu.mainMenu();
+	}
+	
+}
