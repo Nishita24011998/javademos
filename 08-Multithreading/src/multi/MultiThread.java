d164a2ded562a3e502837c9b8bf2ca06f2ccdcb9
nishita24011998 <nishita.cairsc.mathura@gmail.com>
2022-07-31 19:10:16 +0530

    join demo changes

--- 08-Multithreading/src/multi/MultiThread.java
+++ 08-Multithreading/src/multi/MultiThread.java
@@ -3,12 +3,12 @@ package multi;
 public class MultiThread extends Thread{
 	public static void main(String[] args) throws InterruptedException {
 		MultiThread mt = new MultiThread();
-		mt.setName("ilop");
+		//mt.setName("loop ka Ram "); agar loop ka naam rakhna h " Ram "
 		mt.start();
 		
 		String tname = Thread.currentThread().getName();
 		
-		for (int j=1; j<=200; j++) {
+		for (int j=1; j<=10; j++) {
 			System.out.println(tname + ": j: " + j + "\t");
 			Thread.sleep(500);
 		}
@@ -17,7 +17,7 @@ public class MultiThread extends Thread{
 	@Override
 	public void run() {
 		String tname = Thread.currentThread().getName();
-		for (int i=1; i<=200; i++) {
+		for (int i=1; i<=10; i++) {
 			System.out.println(tname + ": i: " + i + "\t");
 			try {
 				Thread.sleep(500);
